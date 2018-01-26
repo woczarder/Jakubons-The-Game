98795b8b7f05533bd978a1d9c0ac41912f9c29e7
woczarder <wojtasessek1997@gmail.com>
2018-01-26 10:35:42 +0100

    Displaying done

--- src/dev/harddrillstudio/jakubons/Game.java
+++ src/dev/harddrillstudio/jakubons/Game.java
@@ -1,4 +1,20 @@
 package dev.harddrillstudio.jakubons;
 
+import dev.harddrillstudio.jakubons.display.Display;
+
 public class Game {
+
+    private Display display;
+
+    private final int width = 800, height = 480;
+
+    public Game() {
+        init();
+    }
+
+    private void init() {
+        /* Create display */
+        display = new Display(width, height);
+    }
+
 }
