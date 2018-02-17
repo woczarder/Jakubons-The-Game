7e8f7f5cfae03104f4e80fe44d5711c6d39cb862
woczarder <wojtasessek1997@gmail.com>
2018-02-17 22:55:48 +0100

    Beginnings of entities system

---
+++ src/dev/harddrillstudio/jakubons/entities/creatures/Player.java
@@ -0,0 +1,25 @@
+package dev.harddrillstudio.jakubons.entities.creatures;
+
+import dev.harddrillstudio.jakubons.Handler;
+import dev.harddrillstudio.jakubons.entities.Entity;
+
+import java.awt.*;
+
+public class Player extends Entity {
+
+    public Player(Handler handler, int x, int y) {
+        super(handler, x, y);
+
+        System.out.println("Player created!");
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics2D g) {
+
+    }
+}
